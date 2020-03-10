2c6a4e246952bf5cf7cd9f044ea31f8da9f5ba4b
apriliandi246 <apriliandi642@gmail.com>
2020-03-10 12:31:54 +0700

    factory code

--- controllers/user_profile.js
+++ controllers/user_profile.js
@@ -5,16 +5,17 @@ const Post = require('../models/Post');
 
 module.exports.render_user_profile_page = async (req, res) => {
       try {
+            // all posts user
             const posts = await Post.find({
                   user_id: req.user._id
             });
 
             res.render('user_profile/user', {
+                  posts,
                   img: req.user.user_picture,
                   name: req.user.name,
                   join: req.user.join,
-                  verified: req.user.verified,
-                  posts: posts
+                  verified: req.user.verified
             });
 
       } catch {
