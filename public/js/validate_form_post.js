f0881d7f56e2ffda8fde295c34ba3edb12fb61bd
apriliandi246 <apriliandi642@gmail.com>
2020-05-03 01:24:20 +0700

    factory code

--- public/js/validate_form_post.js
+++ public/js/validate_form_post.js
@@ -1,27 +1,19 @@
-// ! Have a problem
-// - checking the input value (problem)
-
 const input = document.querySelector('textarea');
 const button = document.querySelector('button');
-const pattern = /^[^\s][a-zA-Z0-9 ]*$/;
+const pattern = /^[^\s][a-zA-Z0-9 ]*$/m;
 
 
 input.addEventListener('keyup', () => {
-    if (pattern.test(input.value) === false) {
+    if (input.value.length > 150) {
         disableButton();
-
-    } else if (input.value.length > 300) {
         input.classList.add('invalid');
-        console.log("lebih goblok");
-        disableButton();
 
-    } else if (input.value === '') {
+    } else if (pattern.test(input.value) === false) {
         disableButton();
 
     } else {
         button.disabled = false;
         button.style.cursor = 'pointer';
-        input.classList.remove('invalid');
     }
 });
 
