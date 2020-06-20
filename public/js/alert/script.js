c324f99cdb695199b459dd17fe5d4cbe37b9b700
apriliandi <apriliandi642@gmail.com>
2020-06-21 02:00:26 +0700

    change name file (css) and factory code

--- public/js/alert/script.js
+++ public/js/alert/script.js
@@ -1,9 +1,8 @@
-const close = document.querySelectorAll('.alert span');
-
-close.forEach(el => {
-   // when each child click
-   el.addEventListener('click', () => {
-      // remove the parent
-      el.parentElement.remove();
+const close = document.querySelectorAll('.alert span')
+   .forEach(el => {
+      // when each child click
+      el.addEventListener('click', () => {
+         // remove the parent
+         el.parentElement.remove();
+      });
    });
-});
